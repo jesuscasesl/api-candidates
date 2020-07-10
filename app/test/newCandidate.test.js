d7d5f5c03a5fef9fcaf8b749028914677287d3b2
Jesus Manuel Castillo Eslava <jcastile@everis.com>
2020-07-10 11:25:48 +0200

    Feature - Validations in middlewares

--- app/test/newCandidate.test.js
+++ app/test/newCandidate.test.js
@@ -66,7 +66,7 @@ describe('POST /candidates', () => {
 
     it('Error not Skills - Bad Request - Skills are required', (done) => {
       const data = {
-        name: 'Name Test'
+        name: 'NameTest'
       }
 
       request(app)
@@ -86,7 +86,7 @@ describe('POST /candidates', () => {
 
     it('Bad Request - Skills must to be an array', (done) => {
       const data = {
-        name: 'Name Test',
+        name: 'NameTest',
         skills: 'php'
       }
 
@@ -98,6 +98,69 @@ describe('POST /candidates', () => {
         .expect(422)
         .then(({ body }) => {
           assert(body.message === 'Bad Request - Skills must to be an array')
+        })
+        .then(() => {
+          server.close()
+          done()
+        })
+    })
+
+    it('Bad Request - The name has more than 100 characters', (done) => {
+      const data = {
+        name: 'Name Test Name Test Name Test Name Test Name Test Name Test Name Test Name Test Name Test Name Test Name',
+        skills: ['php']
+      }
+
+      request(app)
+        .post('/candidates')
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422)
+        .then(({ body }) => {
+          assert(body.message === 'Bad Request - The name has more than 100 characters')
+        })
+        .then(() => {
+          server.close()
+          done()
+        })
+    })
+
+    it('Bad Request - Name is Alphanumeric', (done) => {
+      const data = {
+        name: 'NameTest??',
+        skills: ['php']
+      }
+
+      request(app)
+        .post('/candidates')
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422)
+        .then(({ body }) => {
+          assert(body.message === 'Bad Request - Name is Alphanumeric')
+        })
+        .then(() => {
+          server.close()
+          done()
+        })
+    })
+
+    it('Bad Request - The skills is invalid', (done) => {
+      const data = {
+        name: 'NameTest',
+        skills: ['php', 'p?p', 'js', 'Node']
+      }
+
+      request(app)
+        .post('/candidates')
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422)
+        .then(({ body }) => {
+          assert(body.message === 'Bad Request - The skills is invalid')
         })
         .then(() => {
           server.close()
